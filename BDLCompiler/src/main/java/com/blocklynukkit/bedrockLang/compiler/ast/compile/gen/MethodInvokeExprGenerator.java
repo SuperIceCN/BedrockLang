d359f5f41bae60f3a95549acd494d85ee7a46542
超神的冰凉 <2801621420@qq.com>
2022-01-30 14:24:05 +0800

    修复无法调用接口虚函数问题

--- BDLCompiler/src/main/java/com/blocklynukkit/bedrockLang/compiler/ast/compile/gen/MethodInvokeExprGenerator.java
+++ BDLCompiler/src/main/java/com/blocklynukkit/bedrockLang/compiler/ast/compile/gen/MethodInvokeExprGenerator.java
@@ -3,6 +3,8 @@ package com.blocklynukkit.bedrockLang.compiler.ast.compile.gen;
 import com.blocklynukkit.bedrockLang.compiler.ast.compile.*;
 import com.blocklynukkit.bedrockLang.compiler.ast.compile.impl.piece.MethodInvokeExpr;
 import com.blocklynukkit.bedrockLang.compiler.ast.compile.impl.piece.ReadVariableExpr;
+import com.blocklynukkit.bedrockLang.compiler.ast.compile.type.ClassInfo;
+import com.blocklynukkit.bedrockLang.compiler.ast.util.CommonClassInfo;
 import org.objectweb.asm.MethodVisitor;
 
 import static com.blocklynukkit.bedrockLang.compiler.ast.compile.impl.piece.MethodInvokeExpr.ActionType.New;
@@ -20,7 +22,7 @@ public final class MethodInvokeExprGenerator implements ExprCodeGenerator {
         final GenerateWithASM asmUnit = requireASM(unit);
         final MethodVisitor mv = asmUnit.getCurrentMethodVisitor();
         // 生成函数调用
-        String previousClass = "java/lang/Object";
+        ClassInfo previousClass = CommonClassInfo.Object;
         MethodInvokeExpr.ChainAction[] chainActions = methodInvokeExpr.getChainActions();
         for (int i = 0, chainActionsLength = chainActions.length; i < chainActionsLength; i++) {
             final MethodInvokeExpr.ChainAction each = chainActions[i];
@@ -46,19 +48,23 @@ public final class MethodInvokeExprGenerator implements ExprCodeGenerator {
                     new ReadVariableExpr(methodInvokeExpr.getSourcePos(), methodInvokeExpr, each.getContent()).getCodeGenerator().generate(unit);
                     break;
                 case StaticField:
-                    mv.visitFieldInsn(GETSTATIC, previousClass, each.getContent(), each.getClassInfo().toASMType().getDescriptor());
+                    mv.visitFieldInsn(GETSTATIC, previousClass.getQualifiedName(), each.getContent(), each.getClassInfo().toASMType().getDescriptor());
                     break;
                 case VirtualField:
-                    mv.visitFieldInsn(GETFIELD, previousClass, each.getContent(), each.getClassInfo().toASMType().getDescriptor());
+                    mv.visitFieldInsn(GETFIELD, previousClass.getQualifiedName(), each.getContent(), each.getClassInfo().toASMType().getDescriptor());
                     break;
                 case StaticMethod:
-                    mv.visitMethodInsn(INVOKESTATIC, previousClass, each.getContent(), each.getMethodDescriptor(), false);
+                    mv.visitMethodInsn(INVOKESTATIC, previousClass.getQualifiedName(), each.getContent(), each.getMethodDescriptor(), false);
                     break;
                 case VirtualMethod:
-                    mv.visitMethodInsn(INVOKEVIRTUAL, previousClass, each.getContent(), each.getMethodDescriptor(), false);
+                    if(previousClass.isInterface()){
+                        mv.visitMethodInsn(INVOKEINTERFACE, previousClass.getQualifiedName(), each.getContent(), each.getMethodDescriptor(), true);
+                    }else {
+                        mv.visitMethodInsn(INVOKEVIRTUAL, previousClass.getQualifiedName(), each.getContent(), each.getMethodDescriptor(), false);
+                    }
                     break;
             }
-            previousClass = each.getClassInfo().getQualifiedName();
+            previousClass = each.getClassInfo();
         }
         return methodInvokeExpr.getReturnType();
     }
